e8e746949f5321110050c1f26d0f48ffffe294e5
Guilherme Takakuwa <guilhermetakakuwa@MacBook-Pro-de-Guilherme.local>
2019-01-04 13:47:00 -0200

    quarto app

---
+++ Swoosh/Swoosh/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swoosh
+//
+//  Created by Guilherme Takakuwa on 12/10/18.
+//  Copyright © 2018 Guilherme Takakuwa. All rights reserved.
+//
+
+import UIKit
+
+class TelaPrincipalVC: UIViewController {
+
+    @IBOutlet weak var imagemdoroger: UIImageView!
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var Fundo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func voltarDeHabilidadeVC (unwindSegue: UIStoryboardSegue){
+        
+    }
+}
+
---
+++ new-dev-profile/new-dev-profile/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  new-dev-profile
+//
+//  Created by Guilherme Takakuwa on 12/26/18.
+//  Copyright © 2018 Guilherme Takakuwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var logo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
